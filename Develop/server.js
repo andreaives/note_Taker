76046d4f4a7852e86878152ddceac5915f20e62a
andreaives <andreasue3@gmail.com>
2020-10-23 15:04:11 -0600

    test

--- Develop/server.js
+++ Develop/server.js
@@ -3,8 +3,7 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
-const PORT = process.env.PORT || 3001
-const UUIL = require("uuil")
+const PORT = process.env.PORT || 3000
 app.use(express.static(`public`))
 
 const path = require("path")
