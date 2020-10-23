df5c983da4f5593aac2bc79aca76d82a2bf179a9
andreaives <andreasue3@gmail.com>
2020-10-23 12:55:51 -0600

    set up

--- Develop/server.js
+++ Develop/server.js
@@ -5,6 +5,12 @@ var fs = require("fs");
 var app = express();
 var PORT = process.env.PORT || 3001
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+/// node server js
 app.get("/", function(req,res) {
- res.json(path.join(___dirname, "assets/index.html"))
+res.json(path.join(___dirname, "../assets/js/index.html"))
 }
