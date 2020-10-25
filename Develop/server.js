61bc8f2e0c55057aaae3c3ebe69e7611adb7bf5e
andreaives <andreasue3@gmail.com>
2020-10-25 17:10:09 -0600

    Re-pathing html for css

--- Develop/server.js
+++ Develop/server.js
@@ -16,57 +16,6 @@ const path = require("path")
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// ================== Requiring in route pages==============================
 
-
-// // // ====================  Setting up for PORT  =================================
-// // app.listen(PORT, () => {
-// //  console.log(`listening at http://localhost:${PORT}`)
-// // })
-
-// //=====================  Node server js  ===================================
-// app.get("/", function(req,res) {
-// res.json(path.join(__dirname, "./assets/index.html"))
-// })
-
-
-// // ======================  Show notes page  =================================
-// // app.get("/notes", function(req,res) {
-// //  res.json(path.join(___dirname, "./public/notes.html"))
-// //  })
-
-//  app.listen(PORT, () => {
-//   console.log(`listening at http://localhost:${PORT}`)
-//  })
- 
-//  app.get("api/notes", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/db.json"))
-//  })
-
-// app.get("api/notes", (req, res) => {
-//  res.sendFile(path.join(__dirname, "/db.json"))
-
-// })
-
-
-// router.get("/api/notes", (req, res) => {
-
-// })
-// Sets up the Express app to handle data parsing
-
-
-/// node server js
-app.get("/", function(req,res) {
-res.json(path.join(__dirname, "/public/assets/index.html"))
-})
-
-
-app.get("api/notes", (req, res) => {
- res.sendFile(path.join(__dirname, "/db.json"))
-})
-
-
-// ================== Setting up PORT to listen ============
-
-app.listen(PORT, () => {
- console.log(`listening at http://localhost:${PORT}`)
-})
+require("")
